3f76a535fdaf9a5bc5b8a46086488ff7ee9ba5b2
Marshall-Bits <kz_@hotmail.es>
2025-02-03 10:09:31 +0100

    add tripCardLoading component

---
+++ free-routes-web/components/TripCardLoading.jsx
@@ -0,0 +1,7 @@
+export default function TripCardLoading() {
+  return (
+    <div className="group relative flex h-[420px] w-72 animate-pulse flex-col overflow-hidden rounded-lg bg-white/90 shadow-md sm:w-96">
+      <div className="relative h-2/5 w-full bg-gray-400"></div>
+    </div>
+  );
+}
